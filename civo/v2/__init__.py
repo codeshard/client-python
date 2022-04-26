81c4eaec374bd2fd907b88b0a17df913007b15d2
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2022-04-26 18:48:56 +0200

    feat: civo lib metadata and global imports

---
+++ civo/v2/__init__.py
@@ -0,0 +1,7 @@
+from ._async.client import AsyncCivoClient  # noqa: F401
+from .auth import CivoAuth  # noqa: F401
+from .errors import CivoError  # noqa: F401
+
+__version__ = "1.2.0"
+__author__ = "Alejandro JNM <alejandrojnm@gmail.com>"
+__all__ = []
