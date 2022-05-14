398dcdddbd7f00fca7c33dfc95ebbf5880027d91
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2022-05-14 18:51:00 +0200

    fix: removing unused modules

--- civo/v2/http_clients.py
+++
@@ -1,7 +0,0 @@
-from httpx import AsyncClient  # noqa: F401
-from httpx import Client as BaseClient  # noqa: F401
-
-
-class SyncClient(BaseClient):
-    def aclose(self) -> None:
-        self.close()
