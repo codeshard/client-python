71e9d72ca8cabb0b0694247a66e85ab61daa04d2
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2022-05-14 19:46:53 +0200

    feat: added ssh keys models

---
+++ civo/v2/models/ssh.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from typing import Any, List
+from uuid import UUID
+
+from civo.v2.utils import parse_json
+
+
+@dataclass
+class Key:
+    id: UUID
+    name: str
+    fingerprint: str
+
+    def __post_init__(self):
+        self.id = UUID(str(self.id))
+        self.name = str(self.name)
+        self.fingerprint = str(self.fingerprint)
+
+    @classmethod
+    def from_json(cls, json: Any) -> "Key":
+        return parse_json(cls, **json)
+
+
+@dataclass
+class SSH:
+    keys: List[Key]
+
+    @classmethod
+    def from_json(cls, json: Any) -> "SSH":
+        keys: List[SSH] = []
+        for item in json:
+            keys.append(SSH.from_json(item))
+        del json
+        return parse_json(cls, keys=keys)
