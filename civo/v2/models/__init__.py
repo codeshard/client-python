2b19ceb851401902b643c5af5099bdc77cd7b1ca
Ozkar L. Garcell <ozkar.garcell@gmail.com>
2022-05-17 12:26:31 +0200

    feat: top module

--- civo/v2/models/__init__.py
+++ civo/v2/models/__init__.py
@@ -1,4 +1,5 @@
 from .error import CivoError  # noqa: F401
+from .instance import Instance, Size  # noqa: F401
 from .quota import Quota  # noqa: F401
-from .regions import Region, RegionFeatures, Regions  # noqa: F401
+from .region import Region, RegionFeatures, Regions  # noqa: F401
 from .ssh import SSH, Key, SSHResponse  # noqa: F401
